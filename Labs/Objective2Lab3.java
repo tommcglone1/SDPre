650cd5a47366e07ca600e6bfe33d6479c1ce0d7b
Tom <tom.mcglone1@yahoo.com>
2022-12-16 13:01:50 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int yearborn = 1994;
+		String monthborn = "October";
+		boolean weekend = false;
+		System.out.println(yearborn);
+		System.out.println(monthborn);
+		System.out.println(weekend);
+	}
+}
