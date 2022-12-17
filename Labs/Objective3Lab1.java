f8fbd2ed1609f8b8657f1593dcb95cf6bc14d90c
Tom <tom.mcglone1@yahoo.com>
2022-12-17 16:47:13 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,7 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1 = 15, num2 =6, num3 = 2;
+		double average = (num1 + num2 + num3)/3;
+		System.out.println(average);
+	}
+}
