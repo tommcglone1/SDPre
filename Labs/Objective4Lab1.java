68cdc318a6248b9f63bfc63c978735d4578eff83
Tom <tom.mcglone1@yahoo.com>
2022-12-19 13:59:43 -0500

    complete objective4lab1

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -20,6 +20,5 @@ public class Objective4Lab1 {
 		System.out.println("My favorite animal is the " + favoriteAnimal + ".");
 		System.out.println("My favorite food is " + favoriteFood + ".");
 		System.out.println("My favorite song is " + favoriteSong + ".");
-			
 	}
 }
