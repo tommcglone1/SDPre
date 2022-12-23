2bcb68b6d486ebe83448eb6d05133a8f89f72e22
Tom <tom.mcglone1@yahoo.com>
2022-12-22 21:20:37 -0500

    objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int count = 1, sum = 0;  
+
+		while(count <= 20) {
+			sum = sum + count;
+			count++;	
+		}	
+		System.out.println(sum);
+	}
+}
