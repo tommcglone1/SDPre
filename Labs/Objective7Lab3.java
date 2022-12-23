3dc567b2ef534f03080ad8aaa2c1e622316820ec
Tom <tom.mcglone1@yahoo.com>
2022-12-23 10:32:21 -0500

    corrected objective7lab3

--- Labs/Objective7Lab3.java
+++ Labs/Objective7Lab3.java
@@ -3,16 +3,16 @@ public class Objective7Lab3 {
 		int counter = 1;
 
 		while(counter <= 20) {
-			System.out.print(counter);
-			counter = counter + 1;
+			if(counter % 2 == 0) {
+			System.out.println(counter + " is even");
+		}
+			else if(counter % 2 != 0) {
+			System.out.println(counter + " is odd");
 
-		if(counter % 2 == 0) {
-			System.out.println(" is odd");
 		}
-		else if(counter % 2 != 0) {
-			System.out.println(" is even");
-		}
-		
+			counter ++;
+
+
 		}
 	}
 }
