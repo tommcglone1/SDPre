36de4f2b0d5fbef77836ad46f34e58941fb76b3a
Tom <tom.mcglone1@yahoo.com>
2022-12-24 14:33:48 -0500

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum= 0 ;
+
+		for (int control = 1; control<=20; control++){
+			sum = sum + control;
+		}
+			System.out.println(sum);
+
+	}
+}
