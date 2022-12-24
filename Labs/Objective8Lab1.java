c5515d5960a338de760018e69985651abfc99f17
Tom <tom.mcglone1@yahoo.com>
2022-12-24 14:14:28 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+		for (int loop1=1; loop1<=10; loop1++){ 
+			System.out.println(loop1);
+		}	
+
+		for (int loop2=10; loop2>=1; loop2--){
+			System.out.println(loop2);
+
+		
+		}
+	}
+}
