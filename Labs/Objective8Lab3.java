d80576753b8057bb746567a81883476c27b09b7d
Tom <tom.mcglone1@yahoo.com>
2022-12-24 14:53:48 -0500

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3 {
+	public static void main(String[] args) {
+		
+
+		for (int counter = 1; counter <=20; counter++){
+			if(counter %2 ==0)
+				System.out.println(counter + " is even");
+			
+			else if(counter %2 !=0)
+				System.out.println(counter + " is odd");	
+			
+		}
+
+	}
+}
