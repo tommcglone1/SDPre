1280b21547b7bb8d25e0052faa0136a35056b5a8
Tom <tom.mcglone1@yahoo.com>
2022-12-26 15:38:57 -0500

    complete objective9lab3

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Objective9Lab3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while(true){
+			printMenu();
+			int selection = scanner.nextInt();
+
+			if(selection == 1) 
+				System.out.println("Hello World");
+			
+			if(selection == 2) 
+				System.out.println("Apple, Banana, Coconut");
+			
+			if(selection == 3) 
+				System.out.println("Goodbye");
+				
+			if(selection == 3)
+				break;	
+		}
+
+		scanner.close();
+
+	}
+
+	public static void printMenu() {
+	System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+	}
+}
