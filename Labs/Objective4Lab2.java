5a14ae217e7ddaf097ee3ff32259df729f6beb0f
Tom <tom.mcglone1@yahoo.com>
2022-12-28 13:31:10 -0500

    complete Objective4Lab2

--- Labs/Objective4Lab2.java
+++ Labs/Objective4Lab2.java
@@ -22,7 +22,7 @@ public class Objective4Lab2 {
 
 		double sumDoubleNumbers = dub1 + dub2 + dub3;
 
-		System.out.println("The sum of " + num1 + " " + "+ " + num2 + " " + "+ " + num3 + " =" + sumWholeNumbers );
+		System.out.println("The sum of " + num1 + " " + "+ " + num2 + " " + "+ " + num3 + " = " + sumWholeNumbers );
 		System.out.println("The sum of " + dub1 + " " + "+ " + dub2 + " " + "+ " + dub3 + " = " + sumDoubleNumbers );
 		keyboard.close();
 		
