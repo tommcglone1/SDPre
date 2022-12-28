27c573e6c46796da938537d31cb402bb954b8577
Tom <tom.mcglone1@yahoo.com>
2022-12-28 13:13:56 -0500

    complete Objective3Lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -4,6 +4,6 @@ public class Objective3Lab3 {
 		int second = 1 + (int) (Math.random() * 6);
 		int third = 1 + (int) (Math.random() * 6);
 		int finalResult = first + second + third;
-		System.out.println(first + "+" + second + "+" + third + "=" + finalResult);
+		System.out.println(first + " + " + second + " + " + third + " = " + finalResult);
 	}
 }
