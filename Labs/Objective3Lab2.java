4e5b0e7d5cec11f87e91242dc89d09430697d5ae
Tom <tom.mcglone1@yahoo.com>
2022-12-28 13:07:47 -0500

    complete Objective3Lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -1,10 +1,9 @@
 public class Objective3Lab2 {
 	public static void main(String [] args) {
 		double side1 = 10.0, side2 = 8.0;
-		double totalSide1 = side1 * side1;
-		double totalSide2 = side2 * side2;
-		double totalBothSides = (totalSide1 + totalSide2);
-		double hypotenuse = Math.sqrt(totalBothSides);
+		side1 *= side1; side2 *= side2;
+		double hypotenuse = Math.sqrt(side1 + side2);
 		System.out.println("The hypotenuse of a triangle with sides 10.0 and 8.0 is "+hypotenuse);
+			
 	} 
 }
