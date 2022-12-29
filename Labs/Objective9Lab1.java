03af342624229743e00c001f058f2af542306ab9
Tom <tom.mcglone1@yahoo.com>
2022-12-29 13:11:49 -0500

    complete Objective9Lab1

--- Labs/Objective9Lab1.java
+++ Labs/Objective9Lab1.java
@@ -3,8 +3,8 @@ public class Objective9Lab1 {
 		printName();
 	}
 
-public static void printName() {
-	System.out.println("Hello Doug");
+	public static void printName() {
+		System.out.println("Hello Doug");
 	
 	}
 }
